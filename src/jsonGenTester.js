98fdee8f6c796e9139fd6950592a27069b69d982
Jie Li <jie.li@kingfisher.com>
2024-07-25 14:23:00 +0100

    using json-templater

--- src/jsonGenTester.js
+++ src/jsonGenTester.js
@@ -1,8 +1,10 @@
-// $kube-namespace
-// $kube-service
-
 // Read in dc-project/*/routes.js
 import fs from 'fs';
+import object from 'json-templater/object.js'
+
+import widget from './templates/widget.json' assert { type: 'json' }
+import templateVariables from './templates/variables.json' assert { type: 'json' }
+import dashboard from './templates/dashboard.json' assert { type: 'json' }
 
 // Read in routes.js
 const statuses = {
@@ -21,6 +23,7 @@ httpRoutes.splice(0, 1)
 console.log(httpRoutes.length)
 
 let combined = []
+let aWidget
 // Check each array with command and url
 httpRoutes.forEach(route => {
    const lines = route.split('\n')
@@ -37,6 +40,15 @@ httpRoutes.forEach(route => {
    const JsonObj2 = JSON.parse(query2)
    const JsonObj3 = JSON.parse(query3)
 
+   // render a widget - a test sample
+   aWidget = object(
+      widget,
+      {
+         id: 6748651480114544,
+         title: 'Response Time (s)',
+         query: JsonObj1
+      }
+)
    console.log(JsonObj1, JsonObj2, JsonObj3)
    combined = combined.concat(JsonObj1, JsonObj2, JsonObj3)
 
@@ -46,8 +58,28 @@ httpRoutes.forEach(route => {
 // GET /static/js/app.js
 // const jsonString = '{"req.url":"/static/js/app.js", "req.method":"GET"}'
 
-const outputStream = JSON.stringify(combined, null, 2)
-console.log(outputStream)
+const routes = JSON.stringify(combined, null, 2)
+console.log(routes)
+
+// render variables
+const variables = object(
+   templateVariables,
+   {
+      service: 'isf',
+      namespace: 'dc-production'
+   }
+)
+// render dashboard template
+const outputStream = JSON.stringify(
+   object(
+   dashboard,
+   { 
+      title: 'Jies test board', 
+      description: 'isf routes',
+      widgets: aWidget,  // make it an array
+      variables: variables }
+ ), null, 2)
+ console.log(outputStream)
 
 // file ouput
 try {
